bf30bb723c2f671750e3fe93b1232bb1341cea11
Fabrice Laporte <fabrice@yescapa.com>
2018-11-03 21:48:57 +0100

    reorganize project

---
+++ bang.py
@@ -0,0 +1,80 @@
+import logging
+import time
+from rtmbot.core import Plugin
+
+from memedict import search
+
+from utils import delete_line
+from auth import (USERS_TOKENS, BOT_TOKEN)
+
+
+class BangPlugin(Plugin):
+    """Bang !shortcuts for slack.
+    """
+
+    def process_message(self, data):
+        logging.basicConfig(filename='rtmbotf.log',
+                            format='%(asctime)s %(message)s')
+        tokens = data['text'].split(' ')
+        command = tokens[0]
+        data['text'] = ' '.join(tokens[1:])
+        if command == '!k':
+            self._kaomoji(data)
+        elif command == '!kym':
+            self._kym(data)
+
+    def delete_line(self, data):
+        # Delete command
+        self.slack_client.api_call("chat.delete",
+            token=USERS_TOKENS.get(data['user'], BOT_TOKEN),
+            channel=data['channel'], ts=data['ts'], as_user=True)
+
+    def react(self, data, emoji):
+        self.slack_client.api_call("reactions.add",
+            token=BOT_TOKEN, as_user=False, name=emoji, icon_emoji=':slack-cli:',
+            channel=data['channel'], timestamp=data['ts'])
+
+    # Commands below
+
+    def _help(self, data):
+        """Show this help message.
+        """
+        commands = [x for x in dir(self) if x.startswith('_') and not x.startswith('__')]
+        for cmd in sorted(commands):
+            print(cmd.__doc__)
+
+    def _bomb(self, data):
+        """!b: destruct message after 10 seconds.
+        """
+        time.sleep(7)
+        self.react('three')
+        time.sleep(1)
+        self.react('two')
+        time.sleep(1)
+        self.react('one')
+        time.sleep(1)
+        self.react('fire')
+        self.delete_line(data)
+
+    def _kaomoji(self, data):
+        """!k: replace emoji with kaomoji.
+        """
+        from rsrc.kaomoji import KAOMOJIS
+
+        if data['text'] in KAOMOJIS:
+            delete_line(data)
+            self.slack_client.api_call("chat.postMessage",
+                token=USERS_TOKENS.get(data['user'], BOT_TOKEN),
+                as_user=True, text=KAOMOJIS[data['text']], channel=data['channel'])
+
+    def _kym(self, data):
+        """!kym: print description of given emoji.
+        """
+        emoji = data['text'].split(':')[1]
+        term = ' '.join(emoji.split('_'))
+        description = search(term)
+        self.slack_client.api_call("chat.postMessage", icon_emoji=':%s:' % emoji,
+            token=BOT_TOKEN, as_user=False,
+            username=description.split('.')[0],
+            text='.'.join(description.split('.')[1:]),
+            channel=data['user'])
