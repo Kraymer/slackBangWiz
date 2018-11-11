7263a0106e515015f81473c856847aaf01eb408a
Fabrice Laporte <fabrice@yescapa.com>
2018-11-11 22:24:20 +0100

    add random cmd

--- bang/slacker.py
+++ bang/slacker.py
@@ -6,18 +6,29 @@ SLACK_CLIENT = None  # set at bang plugin creation
 
 
 def init_client(slack_client):
+    """Setup global slack client.
+    """
     global SLACK_CLIENT
     SLACK_CLIENT = slack_client
 
 
-def delete_line(slack_client, data):
-    # Delete command
+def delete_line(data):
+    """Delete command
+    """
     SLACK_CLIENT.api_call("chat.delete",
         token=USERS_TOKENS.get(data['user'], BOT_TOKEN),
         channel=data['channel'], ts=data['ts'], as_user=True)
 
+def channels_info(channel=None):
+    """Get channel infos.
+    """
+    SLACK_CLIENT.api_call("channels.info",
+        token=USERS_TOKENS.get(data['user'], BOT_TOKEN),
+        channel=channel or data['channel'])
 
-def react(slack_client, data, emoji):
+def react(data, emoji):
+    """Add reaction.
+    """
     return SLACK_CLIENT.api_call("reactions.add",
         token=BOT_TOKEN, as_user=False, name=emoji, icon_emoji=':slack-cli:',
         channel=data['channel'], timestamp=data['ts'])
